e084fcf6f8642d5510f0f8fe86133cbabd82aef5
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-06-08 16:54:54 +0200

    aggiunto rilevamento mano

---
+++ index2.js
@@ -0,0 +1,102 @@
+import('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0').then(module => {
+    const { HandLandmarker, FilesetResolver } = module;
+    // Your code here
+    let handLandmarker = undefined
+    let runningMode = 'IMAGE'
+    // let demosSection= document.getElementById('demos')
+
+    const createHandLandmarker = async () => {
+        const vision = await FilesetResolver.forVisionTasks(
+            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+        );
+        handLandmarker = await HandLandmarker.createFromOptions(vision, {
+            baseOptions: {
+                modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
+                delegate:'GPU'
+            },
+            runningMode: runningMode,
+            numHands:2
+        })
+    }
+    createHandLandmarker()
+    //    ********************************************************************
+// Demo 1: Grab a bunch of images from the page and detection them
+// upon click.
+//********************************************************************
+
+// In this demo, we have put all our clickable images in divs with the
+// CSS class 'detectionOnClick'. Lets get all the elements that have
+// this class.
+const imageContainers = document.getElementsByClassName("detectOnClick");
+
+// Now let's go through all of these and add a click event listener.
+for (let i = 0; i < imageContainers.length; i++) {
+  // Add event listener to the child element whichis the img element.
+  imageContainers[i].children[0].addEventListener("click", handleClick);
+}
+// When an image is clicked, let's detect it and display results!
+  async function handleClick(event) {
+    if (!handLandmarker) {
+      console.log("Wait for handLandmarker to load before clicking!");
+      return;
+    }
+  
+    if (runningMode === "VIDEO") {
+      runningMode = "IMAGE";
+      await handLandmarker.setOptions({ runningMode: "IMAGE" });
+    }
+    // Remove all landmarks drawed before
+    const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
+    for (var i = allCanvas.length - 1; i >= 0; i--) {
+      const n = allCanvas[i];
+      n.parentNode.removeChild(n);
+    }
+  
+    // We can call handLandmarker.detect as many times as we like with
+    // different image data each time. This returns a promise
+    // which we wait to complete and then call a function to
+    // print out the results of the prediction.
+    const handLandmarkerResult = handLandmarker.detect(event.target);
+    console.log(handLandmarkerResult.handednesses[0][0]);
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("class", "canvas");
+    canvas.setAttribute("width", event.target.naturalWidth + "px");
+    canvas.setAttribute("height", event.target.naturalHeight + "px");
+    canvas.style =
+      "left: 0px;" +
+      "top: 0px;" +
+      "width: " +
+      event.target.width +
+      "px;" +
+      "height: " +
+      event.target.height +
+      "px;";
+  
+    event.target.parentNode.appendChild(canvas);
+    const cxt = canvas.getContext("2d");
+    for (const landmarks of handLandmarkerResult.landmarks) {
+      drawConnectors(cxt, landmarks, HAND_CONNECTIONS, {
+        color: "#00FF00",
+        lineWidth: 5
+      });
+      drawLandmarks(cxt, landmarks, { color: "#FF0000", lineWidth: 1 });
+    }
+    }
+
+});
+
+
+
+
+
+
+// const camera = document.getElementById('video')
+
+// function camera_computer() {
+//     navigator.getUserMedia(
+//         { video: { width: 1000, height: 1000 } },
+//         stream => camera.srcObject = stream,
+//         error => console.error(error),
+//     )
+// }
+
