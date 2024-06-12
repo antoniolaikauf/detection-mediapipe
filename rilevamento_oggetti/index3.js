3ed227cf4c9ca94d26ad806ed4f9b354a1ad95a4
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-06-12 20:55:35 +0200

    start cordinate in html

--- rilevamento_oggetti/index3.js
+++ rilevamento_oggetti/index3.js
@@ -1,17 +1,18 @@
-import('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2').then(module => { // si importa il moduli dal link e dopo si prendono solo i due valori interessano (HandLandmarker, FilesetResolver)
-    const { ObjectDetector, FilesetResolver, Detection, ObjectDetectionResult } = module;
+import('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2').then(module => { // si importa mediapipe dal link e dopo si prendono i valori che servono 
+    const { ObjectDetector, FilesetResolver} = module;
 
     let objectDetector = undefined
     let runningMode = 'IMAGE'
     const initializeObjectDetector = async () => {
-        const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm");
+        const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"); // file per rilevamento visivo
         objectDetector = await ObjectDetector.createFromOptions(vision, {
+            // opzioni di configuramento
             baseOptions: {
                 modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`, // modello algoritmo 
-                delegate:'GPU',
+                delegate:'GPU', // non utilizza la tua gpu
             },
             scoreThreshold: 0.5, // score che da il modello *
-            runningMode: runningMode, //*
+            runningMode: runningMode, // tipo se video o img
         })
     }
     initializeObjectDetector()
@@ -34,7 +35,7 @@ import('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2').then(modul
         const ctx = canvas.getContext('2d')
         ctx.drawImage(imgObject.children[0],0,0)
         imgs_detection.detections.forEach(element => {
-            ctx.rect(element.boundingBox.originX, element.boundingBox.originY, element.boundingBox.width, element.boundingBox.height)
+            ctx.rect(element.boundingBox.originX, element.boundingBox.originY, element.boundingBox.width, element.boundingBox.height) // disegna nel canvas le sagome con le cordinate rilevate 
             ctx.stroke()
         });
     }
